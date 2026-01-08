35019f0b2319889fb1e932bf0a2033d3de247383
Jacqode <45409429+Jacqode@users.noreply.github.com>
2026-01-08 12:13:23 +0100

    Delete analytics.js

--- analytics.js
+++
@@ -1,6 +0,0 @@
-// Simpel, privatlivsvenlig analytics med wake.html
-
-// Wake tracking
-if (window.location.pathname.includes("wake.html")) {
-  fetch("https://api.countapi.xyz/hit/plugpause//wake.html");
-}
